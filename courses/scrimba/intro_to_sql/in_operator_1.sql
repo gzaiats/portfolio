f81322759574e63372c6c6d1429842b4ae9eeddc
Georgia R. Zaiats <gzaiats@hotmail.com>
2026-01-21 20:17:37 -0300

    Create in_operator_1.sql

---
+++ courses/scrimba/intro_to_sql/in_operator_1.sql
@@ -0,0 +1,10 @@
+/*
+	Select the brand, model, condition and year from cars
+		Where the year is 1961, 1963, 1965, 1967 or 1969
+		and the condition is 3 or higher
+		and sold is false
+*/
+SELECT brand, model, condition, year FROM cars
+  WHERE year IN (1961, 1963, 1965, 1967, 1969)
+	AND condition >=3 
+	AND sold IS false;

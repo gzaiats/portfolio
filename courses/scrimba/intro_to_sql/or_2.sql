d9c17cadd596bd0a4467b163f1bcc71e11653819
Georgia R. Zaiats <gzaiats@hotmail.com>
2026-01-21 20:16:57 -0300

    Create or_2.sql

---
+++ courses/scrimba/intro_to_sql/or_2.sql
@@ -0,0 +1,11 @@
+/*
+	Search for columns: brand, model, color, year, price, sold
+		from the table cars
+		where the color is a shade of red
+		or the year is between 1960 and 1969
+		and sold is false
+*/
+
+SELECT brand, model, color, year, price, sold FROM cars
+	WHERE (color LIKE '%red%'
+	OR year BETWEEN 1960 AND 1969) AND sold = False;

cf805600ff00a25369b4ed3d461efc02dd1ba78b
Georgia R. Zaiats <gzaiats@hotmail.com>
2026-01-21 20:20:27 -0300

    Create order_by_1.sql

---
+++ courses/scrimba/intro_to_sql/order_by_1.sql
@@ -0,0 +1,7 @@
+/*
+	Select the brand, model, condition and price from cars
+		order the table by condition in descending order
+		and by price in ascending order
+*/
+SELECT brand, model, condition, price FROM cars
+  ORDER BY condition DESC, price ASC;

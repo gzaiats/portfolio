52b56b9f14f3b87b6c191ec0c42d483be78079d6
Georgia R. Zaiats <gzaiats@hotmail.com>
2026-01-21 20:20:53 -0300

    Create order_by_2.sql

---
+++ courses/scrimba/intro_to_sql/order_by_2.sql
@@ -0,0 +1,12 @@
+/*
+		Select the brand, model, condition and price from cars
+		where the car is not sold
+		and the condition is not 5
+		order the table by condition in descending order
+		and by price in ascending order
+*/
+
+SELECT brand, model, condition, price FROM cars
+	WHERE sold IS FALSE
+	AND condition != 5
+	ORDER BY condition DESC, price;

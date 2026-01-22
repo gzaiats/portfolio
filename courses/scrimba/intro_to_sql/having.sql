1027eb58c750449c057f900958d5d6f7fb9c9eee
Georgia R. Zaiats <gzaiats@hotmail.com>
2026-01-22 11:43:53 -0300

    Create having.sql

---
+++ courses/scrimba/intro_to_sql/having.sql
@@ -0,0 +1,23 @@
+/*
+	Select:
+		* year
+		* a count of cars from that year, aliased as car_count
+		* the maximum price
+		* the minimum price
+	from the table cars
+		where the car has been sold
+	group by year
+		only show years where more than one car has been sold from that year
+	order the result by car_count
+*/
+
+SELECT 
+  year,
+	count(year) AS car_count,
+	MAX(price),
+	MIN(price)
+FROM cars
+  WHERE sold IS true
+	GROUP BY year
+	HAVING count(year) > 1
+ORDER BY car_count;

a2a2350e4548ec083685dcbaba8bd1e72df3010e
Georgia R. Zaiats <gzaiats@hotmail.com>
2026-03-06 14:09:47 -0300

    Create script.js

---
+++ courses/freecodecamp/javascript/labs/trivia_bot/script.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "georgiaBot";
+let botLocation = "Brazil";
+let favoriteLanguage = "C#";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " has syntax similar to other C-based languages like C++, Java, and JavaScript, making it accessible to developers familiar with those languages.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + "'s ease of use and comprehensive Microsoft Learn documentation make it a good language for beginners and professionals alike.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " benefits from the extensive and open-source .NET platform and robust tooling like Visual Studio."
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");

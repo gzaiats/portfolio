6680a8c53c93ce9621400ce0188f7189a58aff63
Georgia R. Zaiats <gzaiats@hotmail.com>
2026-03-06 14:33:56 -0300

    Create script.js

---
+++ courses/freecodecamp/javascript/labs/sentence_maker/script.js
@@ -0,0 +1,21 @@
+let adjective = "smart";
+let noun = "horse";
+let verb = "playing";
+let place = "field";
+let adjective2 = "funny";
+let noun2 = "carrots";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "sweet";
+noun = "dog";
+verb = "walking";
+place = "cottage";
+adjective2 = "weird";
+noun2 = "bananas";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
